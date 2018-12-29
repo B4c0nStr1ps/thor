8ce754d7727e987569cb15ab8a02ce69a2b32700
Andrea Floro <andreafloro@hotmail.it>
2018-12-29 15:24:06 +0100

    implemented base diagnostic namespace for logging

---
+++ src/libs/thor/include/thor/core/diagnostics.h
@@ -0,0 +1,51 @@
+#ifndef THOR_DIAGNOSTICS_H_
+#define THOR_DIAGNOSTICS_H_
+
+#include "platform/platform.h"
+#include "string.h"
+#include "string_utils.h"
+
+#define THOR_TRACE(verbosity, format, ...) \
+{ \
+	Thor::Diagnostics::Trace(__FILE__, __LINE__, verbosity, format,  ##__VA_ARGS__); \
+}
+
+namespace Thor
+{
+	namespace Diagnostics
+	{
+		enum class TraceVerbosity
+		{
+			Information,
+			Warning,
+			Error
+		};
+
+		const ANSICHAR* VerbosityString(TraceVerbosity verbosity)
+		{
+			switch (verbosity)
+			{
+			case TraceVerbosity::Information:
+				return "INFORMATION";
+			case TraceVerbosity::Warning:
+				return "WARNING";
+			case TraceVerbosity::Error:
+				return "ERROR";
+			default:
+				break;
+				return "UNKOWN VERBOSITY";
+			}
+		}
+
+		template<typename ...Args>
+		void Trace(const ANSICHAR* filename, int32 callerLine, TraceVerbosity verbosity, const ANSICHAR* format, Args... args)
+		{
+			Thor::String userMessage = Thor::StringUtils::Format(format, args...);
+			Thor::String message = Thor::StringUtils::Format("[%s] %s\n|--File: %s Line: %d\n", VerbosityString(verbosity), userMessage.RawData(), filename, callerLine);
+			OutputDebugStringA(message.RawData());
+		}
+
+	} // namespace Diagnostics.
+} // namespace Thor.
+
+#endif // THOR_DIAGNOSTICS_H_
--- src/libs/thor/source/engine.cpp
+++ src/libs/thor/source/engine.cpp
@@ -1,8 +1,10 @@
 #include "thor/engine.h"
 #include "thor/core/platform/platform_application.h"
+#include "thor/core/diagnostics.h"
 
 int32 Thor::ThorMain(HINSTANCE hInInstance, HINSTANCE hPrevInstance, char *, int32 nCmdShow)
 {
+	THOR_TRACE(Diagnostics::TraceVerbosity::Information, "Test logging %s", "My param");
 	auto app = Thor::Platform::Application::CreateApplication();
 	app->Initialize();
 	app->BeginLoop();
